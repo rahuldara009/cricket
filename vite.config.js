3dad1e3094cf81cbfc1195ae6bcac1251c4e09a2
rahuldara009 <dararahul.techfest@gmail.com>
2024-07-07 23:17:04 +0530

    Add production build and configure Vite for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+import { VitePluginGhPages } from 'vite-plugin-gh-pages';
 
 export default defineConfig({
   base: '/cricket/', // Replace 'cricket' with your GitHub repository name
   
   plugins: [
     react(),
+    VitePWA(),
+    VitePluginGhPages({
+      branch: 'gh-pages',
+      push: true,
+      dir: 'dist', // Your build output directory
+    }),
   ],
 });
